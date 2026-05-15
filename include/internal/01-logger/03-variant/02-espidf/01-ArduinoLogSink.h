eb5135d6964d2f1ddf61d1fce36d1dbe1b26dcc1
Nayan <geeknayan@gmail.com>
2026-05-15 17:05:54 +0530

    jkdsjka

--- include/internal/01-logger/03-variant/02-espidf/01-ArduinoLogSink.h
+++ include/internal/01-logger/03-variant/02-espidf/01-ArduinoLogSink.h
@@ -16,32 +16,6 @@ class EspidfLogSink final : public ILogSink {
     Public Virtual ~EspidfLogSink() override = default;
 
     Public Virtual Void WriteLog(CStdString& message) override {
-      /*  time_t nowSec = time(nullptr);
-        // Only use time() for key when NTP has synced (year 2001+); otherwise use millis() so publish can convert later
-        const time_t kMinValidEpoch = 978307200;  // 2001-01-01 00:00:00 UTC
-        Bool timeValid = (nowSec != (time_t)-1 && nowSec >= kMinValidEpoch);
-
-        char timeBuf[24];
-        if (nowSec != (time_t)-1 && nowSec > 0) {
-            struct tm* t = localtime(&nowSec);
-            if (t && strftime(timeBuf, sizeof(timeBuf), "%Y-%m-%d %H:%M:%S", t) > 0) { /* ok *//* }
-            else snprintf(timeBuf, sizeof(timeBuf), "(time?)");
-        } else {
-            snprintf(timeBuf, sizeof(timeBuf), "(no time)");
-        }
-        Serial.print("[");
-        Serial.print(timeBuf);
-        Serial.print("] ");
-        Serial.println(message.c_str());
-
-        static ULong seqPerSec = 0;
-        ULongLong key;
-        if (timeValid) {
-            key = (ULongLong)nowSec * 1000ULL + (ULong)(seqPerSec++ % 1000);
-        } else {
-            key = (ULongLong)millis() * 1000ULL + (ULong)(seqPerSec++ % 1000);
-        }
-        logBuffer->AddLog(key, message);*/
     }
 };
 
