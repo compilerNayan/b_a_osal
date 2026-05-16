542df7307a30c1c01c477b22115158058047457a
Nayan <geeknayan@gmail.com>
2026-05-16 16:27:14 +0530

    kjdsak

--- script/enable_exceptions.py
+++ script/enable_exceptions.py
@@ -1,4 +1,17 @@
 # enable_exceptions.py
 Import("env")
+
+print(">>> [enable_exceptions.py] Running pre-build script...")
+
+# Add -fexceptions
 env.Append(CXXFLAGS=["-fexceptions"])
-env.Replace(CXXFLAGS=[f for f in env.get("CXXFLAGS") if f != "-fno-exceptions"])
+print(">>> [enable_exceptions.py] Added -fexceptions")
+
+# Remove -fno-exceptions if present
+old_flags = env.get("CXXFLAGS")
+new_flags = [f for f in old_flags if f != "-fno-exceptions"]
+if len(new_flags) != len(old_flags):
+    print(">>> [enable_exceptions.py] Removed -fno-exceptions")
+env.Replace(CXXFLAGS=new_flags)
+
+print(">>> [enable_exceptions.py] Final CXXFLAGS:", env.get("CXXFLAGS"))
