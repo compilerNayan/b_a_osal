f047940755fc821be124afc601ffe6df7816b711
Nayan <geeknayan@gmail.com>
2026-05-17 17:42:53 +0530

    jkdas

---
+++ include/internal/01-logger/03-variant/02-espidf/01-EspidfLogSink.h
@@ -0,0 +1,50 @@
+#ifdef ESP_PLATFORM
+#ifndef ESPIDF_LOGSINK_H
+#define ESPIDF_LOGSINK_H
+
+#include <StandardDefines.h>
+#include "../../02-interface/02-ILogSink.h"
+//#include "../../02-interface/02-ILogBuffer.h"
+
+/* @Component */
+class EspidfLogSink final : public ILogSink {
+    /*-- @Autowired */
+    //Private ILogBufferPtr logBuffer;
+
+    Public EspidfLogSink() = default;
+
+    Public Virtual ~EspidfLogSink() override = default;
+
+    Public Virtual Void WriteLog(CStdString& message) override {
+        time_t nowSec = time(nullptr);
+        // Only use time() for key when NTP has synced (year 2001+); otherwise use millis() so publish can convert later
+        const time_t kMinValidEpoch = 978307200;  // 2001-01-01 00:00:00 UTC
+        Bool timeValid = (nowSec != (time_t)-1 && nowSec >= kMinValidEpoch);
+
+        char timeBuf[24];
+        if (nowSec != (time_t)-1 && nowSec > 0) {
+            struct tm* t = localtime(&nowSec);
+            if (t && strftime(timeBuf, sizeof(timeBuf), "%Y-%m-%d %H:%M:%S", t) > 0) { /* ok */ }
+            else snprintf(timeBuf, sizeof(timeBuf), "(time?)");
+        } else {
+            snprintf(timeBuf, sizeof(timeBuf), "(no time)");
+        }
+        printf("[");
+        printf(timeBuf);
+        printf("] ");
+        printf(message.c_str());
+
+        static ULong seqPerSec = 0;
+        ULongLong key;
+        if (timeValid) {
+            key = (ULongLong)nowSec * 1000ULL + (ULong)(seqPerSec++ % 1000);
+        } else {
+            key = (ULongLong)millis() * 1000ULL + (ULong)(seqPerSec++ % 1000);
+        }
+        //logBuffer->AddLog(key, message);
+    }
+};
+
+#endif // ESPIDF_LOGSINK_H
+#endif // ESP_PLATFORM
+
