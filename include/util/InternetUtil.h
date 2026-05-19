d053d53afce22d9847d5cc60ef4b909b2d33d95c
Nayan <geeknayan@gmail.com>
2026-05-19 16:47:17 +0530

    jksdjka

---
+++ include/util/InternetUtil.h
@@ -0,0 +1,60 @@
+#ifndef INTERNET_UTIL_H
+#define INTERNET_UTIL_H
+
+#include <StandardDefines.h>
+#include "esp_netif.h"
+#include "lwip/sockets.h"
+#include "lwip/netdb.h"
+#include "esp_log.h"
+
+class InternetUtil {
+    
+    Public Static Bool IsHostReachable(CStdString host, Int port, Int timeout_ms) {
+        // Check if any netif is up
+        esp_netif_t* netif = esp_netif_next(NULL);
+        bool any_up = false;
+        while (netif) {
+            if (esp_netif_is_netif_up(netif)) {
+                any_up = true;
+                break;
+            }
+            netif = esp_netif_next(netif);
+        }
+        if (!any_up) {
+            ESP_LOGW(TAG, "No network interface is up");
+            return false;
+        }
+    
+        // Try to connect
+        struct addrinfo hints = {};
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = SOCK_STREAM;
+    
+        struct addrinfo* res;
+        if (getaddrinfo(host.c_str(), NULL, &hints, &res) != 0 || res == NULL) {
+            return false;
+        }
+        ((struct sockaddr_in*)res->ai_addr)->sin_port = htons(port);
+    
+        int sock = socket(res->ai_family, res->ai_socktype, 0);
+        if (sock < 0) {
+            freeaddrinfo(res);
+            return false;
+        }
+    
+        struct timeval tv;
+        tv.tv_sec = timeout_ms / 1000;
+        tv.tv_usec = (timeout_ms % 1000) * 1000;
+        setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+        setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, &tv, sizeof(tv));
+    
+        bool connected = (connect(sock, res->ai_addr, res->ai_addrlen) == 0);
+    
+        close(sock);
+        freeaddrinfo(res);
+        return connected;
+    }
+    
+};
+
+#endif // INTERNET_UTIL_H
